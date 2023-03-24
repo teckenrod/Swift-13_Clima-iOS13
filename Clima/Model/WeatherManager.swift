3a45ca1cba8880c1d6a0b1bb76efab17e8a9662a
teckenrod <treyeck2@gmail.com>
2023-03-23 19:38:06 -0500

    first steps

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,41 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Trey Eckenrod on 3/23/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=80089a639b3df7cf2decb318d5e1a05a&units=imperial"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+            task.resume()
+        }
+        
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+        }
+    }
+    
+}
